42b071b759c28cbf53ff62ee5a5a6e5ae0c5f627
Sodium8 <yaroslavostrcov16@gmail.com>
2024-08-13 06:15:45 +0500

    dd

---
+++ src/main/java/net/sodium/testingmod/TeatModClient.java
@@ -0,0 +1,10 @@
+package net.sodium.testingmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TeatModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sodium/testingmod/TestingMod.java
@@ -0,0 +1,16 @@
+package net.sodium.testingmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestingMod implements ModInitializer {
+	public static final String MOD_ID = "testing-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
